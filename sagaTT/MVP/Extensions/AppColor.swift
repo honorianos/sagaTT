e0880a4348447e3e90b5ca4bb0517381113583fc
Oswaldo Escobar <oswaldojeisson.escobarhuisa@everis.nttdata.com>
2022-05-12 06:29:34 -0500

    feat uis

---
+++ sagaTT/MVP/Extensions/AppColor.swift
@@ -0,0 +1,15 @@
+//
+//  AppColor.swift
+//  sagaTT
+//
+//  Created by Oswaldo Jeisson Escobar Huisa on 12/05/22.
+//
+
+import UIKit
+
+struct AppColor {
+    
+    static let Border        = UIColor(named: "Border")!
+    static let Theme        = UIColor(named: "Theme")!
+    static let Navigation    = UIColor(named: "Navigation")!
+}
---
+++ sagaTT/MVP/Extensions/UITextField+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UITextField+Extension.swift
+//  sagaTT
+//
+//  Created by Oswaldo Jeisson Escobar Huisa on 12/05/22.
+//
+
+
+import UIKit
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+extension UITextField {
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------
+    func setLeftPadding(value: Int) {
+
+        self.leftView = UIView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: value, height: value)))
+        self.leftViewMode = .always
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------
+    func setRightPadding(value: Int) {
+
+        self.rightView = UIView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: value, height: value)))
+        self.rightViewMode = .always
+    }
+}
